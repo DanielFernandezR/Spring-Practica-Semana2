88ed269dccafd55b4c0de2ac91528f4b3c91f31c
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-05-06 17:47:10 +0200

    feat(ControllerContactos): Crear método obtenerTelefonoContacto + test

--- src/main/java/org/formacio/mvc/ControllerContactos.java
+++ src/main/java/org/formacio/mvc/ControllerContactos.java
@@ -3,8 +3,10 @@ package org.formacio.mvc;
 import org.formacio.repositori.AgendaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,6 +19,12 @@ public class ControllerContactos {
 	@ResponseBody
 	public int numeroTotalContactos() {
 		return agenda.nombreContactes();
-	} 
+	}
+	
+	@RequestMapping(path="/telefon", method = RequestMethod.GET)
+	@ResponseBody
+	public String obtenerTelefonoContacto(@RequestParam String id) {
+		return agenda.recupera(id).getTelefon();
+	}
 	
 }
